aca45d7fc2b38820b0c4877af63d6874d6602842
DK\IRO <iro@sitecore.net>
2018-02-02 17:54:27 +0200

    Initial

---
+++ src/Sitecore.Support.11395/LayoutsPageContext.cs
@@ -0,0 +1,24 @@
+﻿namespace Sitecore.Support.XA.Foundation.PlaceholderSettings.Services
+{
+  using Microsoft.Extensions.DependencyInjection;
+  using Sitecore.Data.Items;
+  using Sitecore.DependencyInjection;
+  using Sitecore.XA.Foundation.PlaceholderSettings;
+  using Sitecore.XA.Foundation.SitecoreExtensions.Extensions;
+  using System.Linq;
+
+  public class LayoutsPageContext : Sitecore.XA.Foundation.PlaceholderSettings.Services.LayoutsPageContext
+  {
+    public override Item GetSxaPlaceholderItem(string phKey, Item currentItem)
+    {
+      Item placeholderSettingsFolder = ServiceLocator.ServiceProvider.GetService<IPlaceholderSettingsContext>().GetPlaceholderSettingsFolder(currentItem);
+      if (placeholderSettingsFolder == null)
+      {
+        return null;
+      }
+      return (from i in placeholderSettingsFolder.Axes.GetDescendants()
+              where i.InheritsFrom(Templates.Placeholder.ID)
+              select i).FirstOrDefault((Item item) => ((BaseItem)item)[Templates.Placeholder.Fields.PlaceholderKey].Equals(phKey));
+    }
+  }
+}
---
+++ src/Sitecore.Support.11395/SupportRegisterLayoutPage.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Sitecore.DependencyInjection;
+using Sitecore.XA.Foundation.PlaceholderSettings.Services;
+
+namespace Sitecore.Support
+{
+  public class RegisterDependencies : IServicesConfigurator
+  {
+    public void Configure(IServiceCollection serviceCollection)
+    {
+      serviceCollection.AddTransient<ILayoutsPageContext, Sitecore.Support.XA.Foundation.PlaceholderSettings.Services.LayoutsPageContext>();
+    }
+  }
+}
