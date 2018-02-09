b2a70de174dc426d327b0e4775c450af0c380ad9
AntonKechashin <atk@sitecore.net>
2018-02-09 13:28:29 +0200

    Patch version for 1.5

---
+++ src/Sitecore.Support.11395/XA/Foundation/PlaceholderSettings/Pipelines/GetPlaceholderRenderings/GetAllowedRenderings.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Sitecore.Data.Items;
+using Sitecore.Pipelines.GetPlaceholderRenderings;
+using Sitecore.XA.Foundation.PlaceholderSettings;
+using Sitecore.XA.Foundation.SitecoreExtensions.Extensions;
+using System.Linq;
+
+namespace Sitecore.Support.XA.Foundation.PlaceholderSettings.Pipelines.GetPlaceholderRenderings
+{
+  public class GetAllowedRenderings : Sitecore.XA.Foundation.PlaceholderSettings.Pipelines.GetPlaceholderRenderings.GetAllowedRenderings
+  {
+    protected override Item GetSxaPlaceholderItem(GetPlaceholderRenderingsArgs args, Item currentItem)
+    {
+      var placeholderSettings = Sitecore.DependencyInjection.ServiceLocator.ServiceProvider.GetService<IPlaceholderSettingsContext>().GetPlaceholderSettingsFolder(currentItem);
+      #region Added code
+      string placeholderKey = args.PlaceholderKey;
+      int num = args.PlaceholderKey.LastIndexOf('/');
+      if (num >= 0)
+      {
+        placeholderKey = StringUtil.Mid(args.PlaceholderKey, num + 1);
+      } 
+      #endregion
+      var sitePlaceholderItem = placeholderSettings?.Axes.GetDescendants()
+        .Where(i => i.InheritsFrom(Templates.Placeholder.ID))
+        #region Changed code
+        .FirstOrDefault(item => item[Templates.Placeholder.Fields.PlaceholderKey].Equals(placeholderKey));
+        #endregion
+      return sitePlaceholderItem;
+    }
+  }
+}
